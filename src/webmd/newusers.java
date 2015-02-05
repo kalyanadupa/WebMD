b502f3901754815804f2b1d8e274cff8ec0b1f82
kalyanadupa <abhi.klyn@gmail.com>
2015-02-05 07:07:48 +0530

    Added qid with user count (newusers.java) file

--- src/webmd/newusers.java
+++ src/webmd/newusers.java
@@ -27,7 +27,7 @@ import java.util.regex.Pattern;
  *
  * @author Kalyan
  */
-public class webmd_URL_Users {
+public class newusers {
 
     /**
      * @param args
@@ -45,8 +45,8 @@ public class webmd_URL_Users {
         HashMap<String, Integer> staffMap = new HashMap<String, Integer>();
         List<String> uniqueSet = new ArrayList<String>();
         Pattern p8 = Pattern.compile(".{1,50}www.{1,500}|.{1,50}http.{1,500}");
-        String[] fileNames = {"webmd_addiction", "webmd_adhd", "webmd_breast_cancer", "webmd_diabetes", "webmd_diet", "webmd_fkids", "webmd_heart", "webmd_ms", "webmd_pain", "webmd_sexualhealth"};
-//          String[] fileNames = {"webmd_fkids"};
+//        String[] fileNames = {"webmd_addiction", "webmd_adhd", "webmd_breast_cancer", "webmd_diabetes", "webmd_diet", "webmd_fkids", "webmd_heart", "webmd_ms", "webmd_pain", "webmd_sexualhealth"};
+        String[] fileNames = {"webmd_addiction"};
         //*******************************For the matchings of staff and qid**************************/////
 
         for (String fileLog : fileNames) {
@@ -66,8 +66,16 @@ public class webmd_URL_Users {
             staffMap = new HashMap<String, Integer>();
             uniqueSet = new ArrayList<String>();
             BufferedReader br = new BufferedReader(new FileReader("data/" + fileLog + ".csv"));
+            List<String> qidArray = new ArrayList<String>();
             urlUserMap = new HashMap<String, Integer>();
+            BufferedReader in = new BufferedReader(new FileReader("data/" + fileLog + "_staff_qid.csv")); // qid file 
             BufferedWriter writer = new BufferedWriter(new FileWriter("test/" + fileLog + "_PU.txt"));
+
+            String s = in.readLine();
+            
+            while ((s = in.readLine()) != null) {
+                qidArray.add(s);
+            }
 
             while (br.ready()) {
 
@@ -76,65 +84,61 @@ public class webmd_URL_Users {
                 //String nameToken1 = tokenizedTerms1[3];
                 String[] tokenizedTerms = check.split(",");
                 String token = tokenizedTerms[1];
+                System.out.println(token);
                 String nameToken = tokenizedTerms1[3];
 
-                if ((nameToken.length() > 3) && (nameToken.charAt(nameToken.length() - 1) == ' ')) {
-                    nameToken = nameToken.substring(0, nameToken.length() - 1);
-                }
-                //if (check.contains("http") || check.contains("www")) {
-                if (nameToken.toUpperCase().contains(", MSN") || nameToken.toUpperCase().contains(", RNP") || nameToken.toUpperCase().contains(", CDOE") || nameToken.toUpperCase().contains(", MD") || nameToken.toUpperCase().contains(", MPH") || nameToken.toUpperCase().contains(", PHD") || nameToken.toUpperCase().contains(", PT")
-                        || nameToken.toUpperCase().contains(", DSc") || nameToken.toUpperCase().contains(", NCS") || nameToken.toUpperCase().contains(", MSCS") || nameToken.toUpperCase().contains(", RN") || nameToken.toUpperCase().contains("_MSN") || nameToken.toUpperCase().contains("_RNP")
-                        || nameToken.toUpperCase().contains("_CDOE") || nameToken.toUpperCase().contains("_MD") || nameToken.toUpperCase().contains("_MPH") || nameToken.toUpperCase().contains("_PHD") || nameToken.toUpperCase().contains("_PT") || nameToken.toUpperCase().contains("_DSc")
-                        || nameToken.toUpperCase().contains("_NCS") || nameToken.toUpperCase().contains("_MSCS") || nameToken.toUpperCase().contains("_RN")) {
-                    helProPost++;
-                    if (helProMap.get(nameToken) == null) {
-                        helProMap.put(nameToken, 1);
+                if (qidArray.contains(token)) {
+//             	   System.out.println(token);
+
+                    if ((nameToken.length() > 3) && (nameToken.charAt(nameToken.length() - 1) == ' ')) {
+                        nameToken = nameToken.substring(0, nameToken.length() - 1);
+                    }
+                    //if (check.contains("http") || check.contains("www")) {
+                    if (nameToken.toUpperCase().contains(", MSN") || nameToken.toUpperCase().contains(", RNP") || nameToken.toUpperCase().contains(", CDOE") || nameToken.toUpperCase().contains(", MD") || nameToken.toUpperCase().contains(", MPH") || nameToken.toUpperCase().contains(", PHD") || nameToken.toUpperCase().contains(", PT")
+                            || nameToken.toUpperCase().contains(", DSc") || nameToken.toUpperCase().contains(", NCS") || nameToken.toUpperCase().contains(", MSCS") || nameToken.toUpperCase().contains(", RN") || nameToken.toUpperCase().contains("_MSN") || nameToken.toUpperCase().contains("_RNP")
+                            || nameToken.toUpperCase().contains("_CDOE") || nameToken.toUpperCase().contains("_MD") || nameToken.toUpperCase().contains("_MPH") || nameToken.toUpperCase().contains("_PHD") || nameToken.toUpperCase().contains("_PT") || nameToken.toUpperCase().contains("_DSc")
+                            || nameToken.toUpperCase().contains("_NCS") || nameToken.toUpperCase().contains("_MSCS") || nameToken.toUpperCase().contains("_RN")) {
+                        helProPost++;
+                        if (helProMap.get(nameToken) == null) {
+                            helProMap.put(nameToken, 1);
+                        } else {
+                            helProMap.put(nameToken, helProMap.get(nameToken) + 1);
+                        }
+                        continue;
+                    }
+                    if (nameToken.contains("WebMD_Staff")) {
+                        staffPost++;
+
+                        if (staffMap.get(nameToken) == null) {
+                            staffMap.put(nameToken, 1);
+
+                        } else {
+                            staffMap.put(nameToken, staffMap.get(nameToken) + 1);
+                        }
+                        continue;
+                    }
+
+                    if (userMap.get(nameToken) == null) {
+                        userMap.put(nameToken, 1);
+                        if (check.contains("http") || check.contains("www")) {
+                            urlUserCount++;
+                        }
                     } else {
-                        helProMap.put(nameToken, helProMap.get(nameToken) + 1);
+                        userMap.put(nameToken, userMap.get(nameToken) + 1);
+                        if (check.contains("http") || check.contains("www")) {
+                            urlUserCount++;
+                        }
                     }
-                    continue;
+
                 }
-                if (nameToken.contains("WebMD_Staff")) {
-                    staffPost++;
-
-                    if (staffMap.get(nameToken) == null) {
-                        staffMap.put(nameToken, 1);
-
-                    } else {
-                        staffMap.put(nameToken, staffMap.get(nameToken) + 1);
-                    }
-                    continue;
-                }
-
-                if (userMap.get(nameToken) == null) {
-                    userMap.put(nameToken, 1);
-                    if (check.contains("http") || check.contains("www")) {
-                        urlUserCount++;
-                    }
-                } else {
-                    userMap.put(nameToken, userMap.get(nameToken) + 1);
-                    if (check.contains("http") || check.contains("www")) {
-                        urlUserCount++;
-                    }
-                }
-
-                //}
             }
             br.close();
-            //System.out.println("Total Size = "+ userMap.size() +"\n staff Size"+ staffMap.size() +"\n helPro"+ helProMap.size());
 
-//            ValueComparator<String, Integer> comparator = new ValueComparator<String, Integer>(userMap);
-//            Map<String, Integer> sortedUserMap = new TreeMap<String, Integer>(comparator);
-//            sortedUserMap.putAll(userMap);
-//
-//            List<String> sortedList = new ArrayList<String>(sortedUserMap.keySet());
-            //System.out.println(sortedUserMap);
-//            System.out.println(sortedList);
             SortedSet<Map.Entry<String, Integer>> sortedUserMap = entriesSortedByValues(userMap);
-            //System.out.println(sortedUserMap);
+            System.out.println(sortedUserMap);
             br = new BufferedReader(new FileReader("data/" + fileLog + ".csv"));
             int userSize = sortedUserMap.size();
-            //System.out.println("sorted size: " + sortedUserMap.size() +" original size"+userMap.size() );
+//          System.out.println("sorted size: " + sortedUserMap.size() +" original size"+userMap.size() );
             int top25 = userSize / 4;
             int mid50 = userSize - top25 * 2;
             HelPro = helProMap.size();
@@ -147,32 +151,33 @@ public class webmd_URL_Users {
 //            System.out.println("Health Professional User Count: "+ helProMap.size());
 //            System.out.println("Health Professional User Count: "+ helProMap.size());
 //            System.out.println("Staff Members Count: "+ staffMap.size());
-            System.out.println("All users Posts Count :" + addPosts(userMap));
-//              System.out.println("Health Professional Post Count: "+ addPosts(helProMap));
-//              System.out.println("Staff Members Post Count: "+ addPosts(staffMap));
+//              System.out.println("All users Posts Count :" + addPosts(userMap));
+//            System.out.println("Health Professional Post Count: "+ addPosts(helProMap));
+//            System.out.println("Staff Members Post Count: "+ addPosts(staffMap));
 //            System.out.println(staffPost+"  HP--> "+helProPost); 
 //            System.out.println(helProMap.toString());
-            System.out.println(userMap.toString());
+//            System.out.println(userMap.toString());
 
             //         System.out.println("Health Professional User Count: "+ sortedUserMap.toString());
             mid50 = userSize - top25;
             int index = 0;
-            int u1 =0;
+            int u1 = 0;
             int u2 = 0;
             int u3 = 0;
             for (Map.Entry<String, Integer> ent : sortedUserMap) {
-                if(index <= top25)
+                if (index <= top25) {
                     u1 = u1 + ent.getValue();
-                else if((index <= mid50) && (index >top25) )
-                    u2 = u2 +ent.getValue();
-                else if(index > mid50)
+                } else if ((index <= mid50) && (index > top25)) {
+                    u2 = u2 + ent.getValue();
+                } else if (index > mid50) {
                     u3 = u3 + ent.getValue();
-                index ++;
+                }
+                index++;
             }
-            System.out.println("All Posts Count for Q1 "+ u1);
-            System.out.println("All Posts Count for Q2 "+ u2);
-            System.out.println("All Posts Count for Q4 "+ u3);
-            
+//            System.out.println("All Posts Count for Q1 "+ u1);
+//            System.out.println("All Posts Count for Q2 "+ u2);
+//            System.out.println("All Posts Count for Q4 "+ u3);
+
             List<String> sortedUserList = new ArrayList<String>();
             for (Map.Entry<String, Integer> ent : sortedUserMap) {
                 sortedUserList.add(ent.getKey());
@@ -196,12 +201,12 @@ public class webmd_URL_Users {
                 String[] tokenizedTerms = check.split(",");
                 String token = tokenizedTerms[1];
                 String nameToken = tokenizedTerms1[3];
-                
                 if ((nameToken.length() > 3) && (nameToken.charAt(nameToken.length() - 1) == ' ')) {
                     nameToken = nameToken.substring(0, nameToken.length() - 1);
                 }
                 int selectedUserIndex = sortedUserList.indexOf(nameToken);
                 /*
+                 ///////////////////////////////////////////// To get the list of URLS for different Q1, Q2-3, Q4 group///////////////////////////////////////////// 
                  set the condition of the if loop to:
                  For 1st Quartile: (selectedUserIndex < mid50)&&(selectedUserIndex <= top25)&&(selectedUserIndex != -1)
                  For 2nd+3rd Quartile: (selectedUserIndex <= mid50)&&(selectedUserIndex > top25)&&(selectedUserIndex != -1)
@@ -209,8 +214,8 @@ public class webmd_URL_Users {
                  For Health professionals: helProMap.containsKey(nameToken)
                  For WebMD_Staff : staffMap.containsKey(nameToken)
                  */
-//                if((selectedUserIndex <= mid50)&&(selectedUserIndex < top25)&&(selectedUserIndex != -1)){
-                if ((selectedUserIndex < mid50)&&(selectedUserIndex <= top25)&&(selectedUserIndex != -1)) {
+
+                if ((selectedUserIndex < mid50) && (selectedUserIndex <= top25) && (selectedUserIndex != -1) && (qidArray.contains(token))) {
                     userthread++;
                     if ((check.contains("www")) | (check.contains("http"))) {
                         if (urlUserMap.get(nameToken) == null) {
@@ -264,15 +269,16 @@ public class webmd_URL_Users {
 
                 }
 
+                // If loop should end here -- CN
                 //*******************************Find Phenotype (Website Link)**************************/////
             }
             br.close();
             writer.write("Users who posted URL: " + urlUserMap.size() + "\n");
-            System.out.println("Present Group Post Count: " + addPosts(urlUserMap) + "  " + userthread);
+//            System.out.println("Present Group Post Count: " + addPosts(urlUserMap) + "  " + userthread);
             SortedSet<Map.Entry<String, Integer>> sortedJournals = entriesSortedByValues(hashmap);
             for (Map.Entry<String, Integer> ent : sortedJournals) {
                 writer.write(ent.getKey() + "\t" + ent.getValue() + '\n');
-                //System.out.println(ent.getKey() + "\t" + ent.getValue());
+                System.out.println(ent.getKey() + "\t" + ent.getValue());
             }
 
             writer.close();
